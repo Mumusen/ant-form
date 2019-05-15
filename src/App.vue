352b874491f49e5cecefa99ba7b0bfbf0798b128
linxiao <linxiao@pxene.com>
2019-05-15 18:47:25 +0800

    遗漏

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,73 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <a-form :form="form">
+      <a-form-item
+        :label-col="{ span: 3 }"
+        :wrapper-col="{ span: 20 }">
+        <span slot="label">
+          清洗字段&nbsp;
+          <a-tooltip title="线索中必须包含的字段，若某一字段的值未填写、格式错误或语意非法，则线索无效，自动进入归档列表">
+            <a-icon type="question-circle-o" />
+          </a-tooltip>
+        </span>
+        <a-checkbox
+          :indeterminate="clean.indeterminate"
+          :checked="clean.checkAll">
+          全部
+        </a-checkbox>
+        <a-checkbox-group v-decorator="['groupClean', {initialValue: formData.groupClean, rules: [{ required: clean.cleanBtn, message: '请选择清洗字段' }]}]" @change="cleanChange">
+          <a-checkbox class="group-box" v-for="(item, index) in cleanLists" :value="item.id" :key="index">{{item.name}}</a-checkbox>
+        </a-checkbox-group>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      form: this.$form.createForm(this),
+      formData: {
+        groupClean: [1]
+      },
+      clean: {
+        cleanBtn: true,
+        checkAll: false,
+        indeterminate: false
+      },
+      cleanLists: [
+        {
+          id: 1,
+          name: '姓名'
+        }, {
+          id: 2,
+          name: '性别'
+        }, {
+          id: 3,
+          name: '手机号'
+        }, {
+          id: 4,
+          name: '邮箱'
+        }
+      ]
+    }
+  },
+  methods: {
+    cleanChange () {
+      const checkedList = this.form.getFieldValue('groupClean')
+      // 获取旧值问题
+      console.log(this.form.getFieldValue('groupClean'))
+      this.clean.indeterminate = !!checkedList.length && (checkedList.length < this.cleanLists.length - 1)
+      this.clean.checkAll = checkedList.length === this.cleanLists.length - 1
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 #app {
+  margin: 40px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-form :form="form">
+      <a-form-item
+        :label-col="{ span: 3 }"
+        :wrapper-col="{ span: 20 }">
+        <span slot="label">
+          清洗字段&nbsp;
+          <a-tooltip title="线索中必须包含的字段，若某一字段的值未填写、格式错误或语意非法，则线索无效，自动进入归档列表">
+            <a-icon type="question-circle-o" />
+          </a-tooltip>
+        </span>
+        <a-checkbox
+          :indeterminate="clean.indeterminate"
+          @change="onCheckAllChangeClean"
+          :checked="clean.checkAll">
+          全部
+        </a-checkbox>
+        <a-checkbox-group v-decorator="['groupClean', {rules: [{ required: clean.cleanBtn, message: '请选择清洗字段' }]}]" @change="cleanChange">
+          <a-checkbox class="group-box" v-for="(item, index) in cleanLists" :value="item.id" :key="index">{{item.name}}</a-checkbox>
+        </a-checkbox-group>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      form: this.$form.createForm(this),
+      formData: {
+        groupClean: []
+      },
+      clean: {
+        cleanBtn: false,
+        checkAll: false,
+        indeterminate: false
+      },
+      cleanLists: [
+        {
+          id: 1,
+          name: '姓名'
+        }, {
+          id: 2,
+          name: '性别'
+        }, {
+          id: 3,
+          name: '手机号'
+        }, {
+          id: 4,
+          name: '邮箱'
+        }
+      ]
+    }
+  },
+  methods: {
+    onCheckAllChangeClean () {},
+    cleanChange () {
+      // const checkedList = this.form.getFieldValue('groupClean')
+      console.log(this.form.getFieldValue('groupClean'))
+      // this.clean.indeterminate = !!checkedList.length && (checkedList.length < this.cleanLists.length - 1)
+      // this.clean.checkAll = checkedList.length === this.cleanLists.length - 1
+    }
   }
 }
 </script>
